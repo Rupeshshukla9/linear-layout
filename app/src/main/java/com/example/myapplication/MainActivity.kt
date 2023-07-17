543207f35f6e2ebe6d33bdcebd4fe34042b51e38
Rupeshshukla9 <rupeshshukla0321@gmail.com>
2023-07-17 22:09:12 +0530

    linear layout changes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    // variable declaration
+    var etName : EditText ?=null
+    var etRollno : EditText ?=null
+    var etHeight : EditText ?=null
+    var btnValidation : Button ?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //initialization
+        etName = findViewById(R.id.etName)
+
     }
 }
